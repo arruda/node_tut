9d8a7d2be6738d9f3be4421021aa376bde64af2d
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2012-06-07 02:54:13 -0300

    doing more stuff

--- app.js
+++ app.js
@@ -7,9 +7,7 @@ var express = require('express')
   , routes = require('./routes')
   , http = require('http');
 var app = express();
-mongoose = require('mongoose')
-db = mongoose.connect('mongodb://localhost/nodepad');
-Document = require('./models.js').Document(db);
+mongoose = require('mongoose');
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
@@ -24,25 +22,138 @@ app.configure(function(){
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler());
+  app.use(express.errorHandler());  
+  db = mongoose.connect('mongodb://localhost/nodepad');
 });
+
+app.configure('test', function() {
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+  db = mongoose.connect('mongodb://localhost/nodepad-test');
+});
+
+app.Document = Document = require('./models.js').Document(db);
 
 app.get('/', routes.index);
 
-app.get('/documents', function(req, res) {
-  console.log("1");    
+app.get('/documents.:format?', function(req, res) {
+  console.log("in get documents");
   Document.find({}, function(err,docs) {
     if(err) throw err;
 
-    console.log("2");   
-    // 'documents' will contain all of the documents returned by the query
-    res.send(docs.map(function(d) {
-        console.log("3");  
-      // Return a useful representation of the object that res.send() can send as JSON
-      return d.__doc;
-    }));
+    switch (req.params.format) {
+      // When json, generate suitable data
+      case 'json':
+        console.log("so what?");
+        return d;
+      break;
+
+      // Else render a database template (this isn't ready yet)
+      default:
+        console.log("wtf?");
+        res.render('documents/index.jade',{documents:docs})
+    }
+
+  });
+  // var doc = new Document();
+  // doc.title = "doc3";
+  // doc.data = "content3";
+  // doc.tags = "bla, ble";  
+  // doc.save(function(err){
+  //   if(err) throw err;
+
+  //   res.send(doc.title);
+  // });
+});
+
+app.post('/documents.:format?', function(req, res) {
+
+  var document = new Document(req.body['document']);
+  console.log("doc>"+document);
+  document.save(function(err) {
+    if(err){ throw err;}
+
+    switch (req.params.format) {
+      case 'json':
+        console.log("is json");
+        res.send(document);
+       break;
+
+       default:
+        console.log("not json");
+        res.redirect('/documents');
+    }
   });
 });
+
+app.put('/documents/:id.:format?', function(req, res) {
+  // Load the document
+  Document.findById(req.body.document.id, function(err,d) {
+    if(err){throw err;}
+    // Do something with it
+    d.title = req.body.document.title;
+    d.data = req.body.document.data;
+
+    // Persist the changes
+    d.save(function(err) {
+      if(err){throw err;}
+      // Respond according to the request format
+      switch (req.params.format) {
+        case 'json':
+          res.send(d);
+         break;
+
+         default:
+          res.redirect('/documents');
+      }
+    });
+  });
+});
+
+app.del('/documents/:id.:format?', function(req, res) {
+  Document.findById(req.params.id, function(err,d) {
+    if(err){throw err;}
+
+    d.remove(function(err) {
+      if(err){throw err;}
+      switch (req.params.format) {
+        case 'json':
+          res.send('true');
+         break;
+
+         default:
+          res.redirect('/documents');
+      } 
+    });
+  });
+});
+
+app.get('/documents/:id.:format?/edit', function(req, res) {
+  Document.findById(req.params.id, function(err,d) {
+    if(err){throw err;}
+    res.render('documents/edit.jade', { d: d });
+  });
+});
+
+app.get('/documents/new', function(req, res) {
+  res.render('documents/new.jade',{ d: new Document() });
+});
+
+
+// Read document
+app.get('/documents/:id.:format?', function(req, res) {
+  Document.findById(req.params.id, function(err,d) {
+    if(err){throw err;}
+    switch (req.params.format) {
+      case 'json':
+        res.send(d);
+      break;
+
+      default:
+        res.render('documents/show.jade', { d: d });
+    }
+  });
+});
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
---
+++ test/app.test.js
@@ -0,0 +1,65 @@
+
+// Force test environment
+process.env.NODE_ENV = 'test';
+
+var app = require('../app'),
+    lastID = '';
+
+
+module.exports = {
+  'POST /documents.json': function(assert) {
+    assert.response(app, {
+        url: '/documents.json',
+        method: 'POST',
+        data: JSON.stringify({ document: { title: 'Test' } }),
+        headers: { 'Content-Type': 'application/json' }
+      }, {
+        status: 200,
+        headers: { 'Content-Type': 'application/json' }
+      },
+
+      function(res) {
+        var document = JSON.parse(res.body);
+        assert.equal('Test', document.title);
+        lastID = document._id;
+      });
+  },
+
+  'HTML POST /documents': function(assert) {
+    assert.response(app, {
+        url: '/documents',
+        method: 'POST',
+        data: 'document[title]=test',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+      }, {
+        status: 302,
+        headers: { 'Content-Type': 'text/plain' }
+      });
+  },
+
+  'GET /documents.json': function(assert) {
+    assert.response(app,
+      { url: '/documents.json' },
+      { status: 200, headers: { 'Content-Type': 'application/json' }},
+      function(res) {
+        var documents = JSON.parse(res.body);
+        assert.type(documents, 'object')
+
+        documents.forEach(function(d) {
+          app.Document.findById(d._id, function(document) {
+            document.remove();
+          })
+        });
+      });
+  },
+
+  'GET /': function(assert) {
+    assert.response(app,
+      { url: '/' },
+      { status: 200, headers: { 'Content-Type': 'text/html; charset=utf-8' }},
+      function(res) {
+        assert.includes(res.body, '<title>Express</title>');
+        process.exit();
+      });
+  }
+};
